31656b4f25b5d772c01ca7de19f24ea3e3937112
Hanna Negash <hnegash.n@gmail.com>
2023-12-10 18:32:16 -0800

    Remove named variables

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    return list_comp = [n for n in num_list if n%2 == 0]
+    return l[n for n in num_list if n%2 == 0]
 
 def make_exclamation(sentence_list):
-    return list_comp = [s + "!" for s in sentence_list]
+    return [s + "!" for s in sentence_list]
