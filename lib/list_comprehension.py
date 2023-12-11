c3698ccdd8eb51724d12be650c94f7311b37a2b8
Hanna Negash <hnegash.n@gmail.com>
2023-12-10 18:31:12 -0800

    Add return method

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    list_comp = [n for n in num_list if n%2 == 0]
+    return list_comp = [n for n in num_list if n%2 == 0]
 
 def make_exclamation(sentence_list):
-    list_comp = [s + "!" for s in sentence_list]
+    return list_comp = [s + "!" for s in sentence_list]
